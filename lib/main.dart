f3c1f0f6f47765bd9dd6aa7baabe7a381e6883bd
Hamza-Rahmoun <hamza.rahmoun@gmail.com>
2020-02-24 11:46:32 +0100

    clippaintcolor

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,117 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'customeclipper.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: HomePage(),
+      home: HomePage(),
+    );
+  }
+}
+
+Color mainBGColor = Color(0xFF652A78);
+Color redColor = Color(0xFFDE3C10);
+Color purpleColor = Color(0xFF8132AD);
+Color cyan = Color(0xFF99D5E5);
+Color orange = Color(0xFFE97A4D);
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ClipPath(
+        clipper: CustomClipperWidget(),
+        child: CustomPaint(
+          painter: MyCustomPainter(),
+          child: Container(
+            height: 500.0,
+          ),
+        ),
       ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class CustomClipperWidget extends CustomClipper<Path> {
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        ClipPath(
-          clipper: CustomClipperWidget(),
-          child: Container(
-            height: 400,
-            color: Colors.orange,
-          ),
-        )
-      ],
-    );
+  getClip(Size size) {
+    final Path path = Path();
+    path.lineTo(0.0, size.height);
+    var firstEndPoint = Offset(size.width * 0.5, size.height - 30);
+    var firstControlPoint = Offset(size.width * 0.25, size.height - 50);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+    var secondEndPoint = Offset(size.width, size.height - 80);
+    var secondControl = Offset(size.width * 0.75, size.height - 10);
+    path.quadraticBezierTo(secondControl.dx, secondControl.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => true;
+}
+
+class MyCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint();
+
+    Path mainBGPath = Path();
+    mainBGPath.addRect(Rect.fromLTWH(0.0, 0.0, size.width, size.height));
+    paint.color = mainBGColor;
+    canvas.drawPath(mainBGPath, paint);
+
+    Path purplePath = Path();
+    purplePath.lineTo(size.width * .45, 0);
+    purplePath.quadraticBezierTo(
+        size.width * .25, size.height * .3, 0, size.height * 0.55);
+    purplePath.close();
+    paint.color = purpleColor;
+    canvas.drawPath(purplePath, paint);
+
+    Path redPath = Path();
+    redPath.moveTo(size.width * 0.9, 0.0);
+    redPath.quadraticBezierTo(
+        size.width * .5, size.height * 0.1, 0, size.height * 0.85);
+    redPath.lineTo(0, size.height);
+    redPath.lineTo(size.width * 0.25, size.height);
+    redPath.quadraticBezierTo(
+        size.width * .5, size.height * 0.7, size.width, size.height * 0.6);
+    redPath.lineTo(size.width, 0.0);
+    redPath.close();
+    paint.color = redColor;
+    canvas.drawPath(redPath, paint);
+
+    Path orangePath = Path();
+    orangePath.moveTo(0, size.height * 0.75);
+    orangePath.quadraticBezierTo(
+        size.width * .25, size.height * 0.85, size.width * .40, size.height);
+    orangePath.lineTo(0, size.height);
+    orangePath.close();
+    paint.color = orange;
+    canvas.drawPath(orangePath, paint);
+
+    Path trianglePath = Path();
+    trianglePath.lineTo(size.width * .25, 0);
+    trianglePath.lineTo(0, size.height * .25);
+    trianglePath.close();
+    paint.color = cyan;
+    canvas.drawPath(trianglePath, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
   }
 }
